fffc7df220c9a70604f1d3ef01117e4363ac698c
Patrick S <patrick.stampler@gmail.com>
2021-09-16 15:39:39 +0200

    added watermark program

---
+++ image_watermark.py
@@ -0,0 +1,23 @@
+import os
+from PIL import Image, ImageFont, ImageDraw
+import matplotlib.pyplot as plt
+import numpy as np
+
+#
+# Todo: loop to mass process
+#
+directory = 'input'
+
+for filename in os.listdir(directory):
+
+    image = Image.open(os.path.join(directory, filename))
+
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("arial.ttf", 50)
+
+    width, height = image.size
+    draw.text((width - 150, height - 100),
+              "Nevolo", (255, 255, 255), font=font)
+    plt.subplot(1, 2, 1)
+    plt.title("black text")
+    image.save(os.path.join(directory, filename))
