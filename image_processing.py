8786f369887084d113aaa0523f46c68bdf8d9788
Patrick S <patrick.stampler@gmail.com>
2021-09-16 10:46:51 +0200

    init

---
+++ image_processing.py
@@ -0,0 +1,43 @@
+
+import os
+from PIL import Image, ImageEnhance
+
+#
+# Todo: loop to mass process
+#
+directory = 'input'
+
+for filename in os.listdir(directory):
+
+    image = Image.open(os.path.join(directory, filename))
+    # success check
+    # image.show()
+
+    # enhancing the contrast
+    contrast = ImageEnhance.Contrast(image)
+    image = contrast.enhance(1.5)
+
+    # enhancing color
+    color = ImageEnhance.Color(image)
+    image = color.enhance(1.5)
+
+    # hue shifts
+    color = Image.new('RGB', image.size, 'red')
+    image = Image.blend(image, color, 0.1)
+
+    color = Image.new('RGB', image.size, 'yellow')
+    image = Image.blend(image, color, 0.001)
+
+    color = Image.new('RGB', image.size, 'blue')
+    image = Image.blend(image, color, -0.001)
+
+    # enhancing brightness
+    color = ImageEnhance.Brightness(image)
+    image = color.enhance(1.2)
+
+    # enhancing sharpness
+    color = ImageEnhance.Sharpness(image)
+    image = color.enhance(1.5)
+
+    # save the result
+    image.save(os.path.join('output', filename))
